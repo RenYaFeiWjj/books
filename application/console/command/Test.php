ec10a84e61aef4e4032aaccba28362b1d74fb17a
renyafei <18401223106@163.com>
2020-07-17 15:11:42 +0800

    tiiao

---
+++ application/console/command/Test.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * tpAdmin [a web admin based ThinkPHP5]
+ *
+ * @author    yuan1994 <tianpian0805@gmail.com>
+ * @link      http://tpadmin.yuan1994.com/
+ * @copyright 2016 yuan1994 all rights reserved.
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+namespace app\console\command;
+
+use think\console\Command;
+use think\console\Input;
+use think\console\input\Argument;
+use think\console\input\Option;
+use think\console\Output;
+use think\Db;
+
+
+class Test extends Command
+{
+    protected function configure()
+    {
+        $this->setName('test')
+            ->setDescription('定时计划测试：每分钟插入一条数据');
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        // 输出到日志文件
+        $output->writeln("TestCommand:");
+        // 定时器需要执行的内容
+        $output->writeln("TestCommand:");
+
+        $output->writeln("end....");
+    }
+}
