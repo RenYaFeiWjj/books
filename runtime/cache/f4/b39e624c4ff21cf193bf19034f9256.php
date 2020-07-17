7412d1e6c71cf6f5442b06f8fbf9037e14a2e7e3
renyafei <18401223106@163.com>
2020-07-17 13:54:51 +0800

    tj

--- runtime/cache/f4/b39e624c4ff21cf193bf19034f9256.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000001800
- exit();?>
-a:9:{s:8:"books_id";i:20;s:10:"books_name";s:21:"我真没想重生啊";s:12:"books_author";s:15:"柳岸花又明";s:10:"books_time";s:10:"2020-07-15";s:10:"books_type";i:5;s:12:"books_status";i:0;s:14:"books_synopsis";s:346:"万万没想到，社会精英、钻石单身汉的陈汉升居然重生了，一觉醒来变成了高三毕业生。　　十字路口的陈汉升也在犹豫，到底是按部就班成为千万富翁；还是努力一把，在个人资产后面加几个零，并改变历史进程。　　已有作品《大时代1994》，书友群601246758。";s:9:"books_img";s:11:"3318786.jpg";s:9:"books_url";s:50:"https://www.23txt.com/files/article/html/54/54645/";}
