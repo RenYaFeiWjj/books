54979cc7b91ccfcd1781b9b189675234743804e1
renyafei <18401223106@163.com>
2020-07-22 11:21:02 +0800

    tiiao

--- application/console/command/Get.php
+++ application/console/command/Get.php
@@ -694,13 +694,11 @@ class Get extends Command
                             $status = 0;
                             if ($time) {
                                 $time = date('Y-m-d' , strtotime($time));
-                                echo $time;
                                 if (time() - strtotime($time) > 60 * 60 * 24 * 180) {
                                     echo '-----状态改为完本' . PHP_EOL;
                                     $status = 1;
                                 }
                             }
-                            echo '-----状态改为完本'.$status . PHP_EOL;
                             $ress = Db::table('books_cou')->where(['books_id' => $v['books_id'], 'books_status' => $status])->update(['books_time' => $time]);
                             if ($ress) {
                                 echo '-----更新时间更新成功' . PHP_EOL;
