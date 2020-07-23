001ba9287cbcaad1a6eedd00528e54c24a95d8a6
renyafei <18401223106@163.com>
2020-07-23 10:21:26 +0800

    tiiao

--- application/console/command/Get.php
+++ application/console/command/Get.php
@@ -214,10 +214,10 @@ class Get extends Command
         $this->start_time = $this->getCurrentTime();
         echo '------开始咯' . PHP_EOL;
         echo "process-start-time:" . date("Ymd H:i:s") . PHP_EOL;
-
-        echo "采集m.37zw.net" . PHP_EOL;
-        $this->ready($this->config['m.37zw.net']);
-        echo "采集m.biquge5200.cc" . PHP_EOL;
+//
+//        echo "采集m.37zw.net" . PHP_EOL;
+//        $this->ready($this->config['m.37zw.net']);
+//        echo "采集m.biquge5200.cc" . PHP_EOL;
         $this->ready($this->config['m.biquge5200.cc']);
 //        echo "采集起点" . PHP_EOL;
 //        $this->getCaiji($output); //采集笔趣pc端
@@ -226,7 +226,7 @@ class Get extends Command
 //        echo "更新37zw作者" . PHP_EOL;
 //        $this->updateMData($output, 14, 'm.37zw.net'); //更新作者和更新时间
 //        echo '------结束咯' . PHP_EOL;
-        $this->updateChapters();
+//        $this->updateChapters();
 //        $output->writeln("更新成功" . $this->update_count);
 //        $output->writeln("用时" . $this->end_time - $this->start_time);
 
@@ -236,25 +236,25 @@ class Get extends Command
 
     public function ready($config)
     {
-        for ($i = 0; $i < 15; $i++) {
-            if (isset($config['menu'][$i])) {
-                echo '------开始' . $config['menu'][$i]['url'] . PHP_EOL;
-                $process = new \swoole_process(function (\swoole_process $worker) use ($i, $config) {
-                    $this->process($i, $config, $config['menu'][$i]['url']);
-                });
-                $pid = $process->start();
-                echo $config['menu'][$i]['url'] . '------第' . $i . '页个子进程创建完毕' . PHP_EOL;
-            }
-        }
-//        $this->process(1, $config, $config['menu'][2]['url']);
+//        for ($i = 0; $i < 15; $i++) {
+//            if (isset($config['menu'][$i])) {
+//                echo '------开始' . $config['menu'][$i]['url'] . PHP_EOL;
+//                $process = new \swoole_process(function (\swoole_process $worker) use ($i, $config) {
+//                    $this->process($i, $config, $config['menu'][$i]['url']);
+//                });
+//                $pid = $process->start();
+//                echo $config['menu'][$i]['url'] . '------第' . $i . '页个子进程创建完毕' . PHP_EOL;
+//            }
+//        }
+        $this->process(2, $config, $config['menu'][2]['url']);
     }
 
     public function process($k, $config, $url)
     {
-        for ($i = 1; $i < 300; $i++) {
-            echo $k . "------开始第" . $i . "页" . PHP_EOL;
-            $this->search($k, $config, $url . $i . '/');
-        }
+//        for ($i = 1; $i < 300; $i++) {
+//            echo $k . "------开始第" . $i . "页" . PHP_EOL;
+            $this->search($k, $config, $url . 218 . '/');
+//        }
     }
 
 
@@ -342,13 +342,14 @@ class Get extends Command
 
                     //去除前面重复的几个最新章节
                     $match = array_unique_fb($match);
+                    $chapter = [];//218 美女
                     foreach ($match as $key => $val) {
                         //使用该函数对结果进行转码
                         $chapter[$key]['text'] = mb_convert_encoding($val[0], 'UTF-8', 'UTF-8,GBK,GB2312,BIG5');
                         $chapter[$key]['href'] = correct_url($href, $val[1]);
                     }
 
-                    $end_chapter = end($chapter);
+                    $end_chapter = $chapter ? $chapter[0] : [];
                 }
                 $chapter_data = ['books_id' => $books_id, 'chapter_name' => $end_chapter['text'], 'chapter_url' => $end_chapter['href']];
 
