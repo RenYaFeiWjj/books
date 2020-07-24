5d63779aedda4db25f507757b728c7c4cd9f17f9
renyafei <18401223106@163.com>
2020-07-24 14:44:40 +0800

    添加

--- application/console/command/Get.php
+++ application/console/command/Get.php
@@ -635,21 +635,92 @@ class Get extends Command
 
     public function updateChapters()
     {
-        Cache::set('zhang', 0, 3600);
-        echo "process-start-time:" . date("Ymd H:i:s") . PHP_EOL;
+//        Cache::set('zhang', 0, 3600);
+//        echo "process-start-time:" . date("Ymd H:i:s") . PHP_EOL;
+//
+//        $p = Cache::set('p','');
+//        echo $p . '||' . PHP_EOL;
+////        $this->updateChapter(1);
+//        for ($i = 0; $i < 40; $i++) {
+//            echo '------开始' . $i . PHP_EOL;
+//            $process = new \swoole_process(function (\swoole_process $worker) use ($i) {
+//                $this->updateChapter($i);
+//            });
+//            $pid = $process->start();
+////            \swoole_process::wait();
+//            echo '------第' . $i . '页个子进程创建完毕' . PHP_EOL;
+//        }
 
-        $p = Cache::set('p','');
-        echo $p . '||' . PHP_EOL;
-//        $this->updateChapter(1);
-        for ($i = 0; $i < 40; $i++) {
-            echo '------开始' . $i . PHP_EOL;
-            $process = new \swoole_process(function (\swoole_process $worker) use ($i) {
-                $this->updateChapter($i);
-            });
-            $pid = $process->start();
-//            \swoole_process::wait();
-            echo '------第' . $i . '页个子进程创建完毕' . PHP_EOL;
+        $s_time = time();
+        echo '开始时间:'.date('H:i:s',$s_time).PHP_EOL;
+        $work_number=6;
+        $worker=[];
+
+        //模拟地址
+
+        $curl=[
+
+            'https://blog.csdn.net/feiwutudou',
+
+            'https://wiki.swoole.com/wiki/page/215.html',
+
+            'http://fanyi.baidu.com/?aldtype=16047#en/zh/manager',
+
+            'http://wanguo.net/Salecar/index.html',
+
+            'http://o.ngking.com/themes/mskin/login/login.jsp',
+
+            'https://blog.csdn.net/marksinoberg/article/details/77816991'
+
+        ];
+
+        for ($i=0; $i < $work_number; $i++) {
+
+            //创建多线程
+
+            $pro=new \swoole_process(function(\swoole_process $work) use($i,$curl){
+
+                //获取html文件
+
+                $content=$this->curldeta($curl[$i]);
+
+                //写入管道
+
+                $work->write($content.PHP_EOL);
+
+            },true);
+
+            $pro_id=$pro->start();
+
+            $worker[$pro_id]=$pro;
+
         }
+
+        foreach ($worker as $v) {
+
+            echo $v->read().PHP_EOL;
+
+        }
+
+        \swoole_process::wait();
+
+
+        $e_time = time();
+
+        echo '结束时间:'.date('H:i:s',$e_time).PHP_EOL;
+
+
+
+        echo '所用时间:'.($e_time-$s_time).'秒'.PHP_EOL;
+    }
+
+    public function curldeta($curl_arr)
+    {//file_get_contents
+
+        echo $curl_arr.PHP_EOL;
+
+        file_get_contents($curl_arr);
+
     }
 
 
