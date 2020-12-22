6f3e954a7f6ac36b6418be07c6673830103fd15f
renyafei <18401223106@163.com>
2020-12-22 16:16:41 +0800

    tj

--- application/console/command/Get.php
+++ application/console/command/Get.php
@@ -249,7 +249,6 @@ class Get extends Command
                 echo $config['menu'][$i]['url'] . '------第' . $i . '页个子进程创建完毕' . PHP_EOL;
             }
         }
-        $this->process(2, $config, $config['menu'][2]['url']);
     }
 
     public function process($k, $config, $url)
